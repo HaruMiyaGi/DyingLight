c89ebf6a6b37fa40ee2746baf91b1fb1a96eb478
HaruMiyaGi <66522128+HaruMiyaGi@users.noreply.github.com>
2020-10-11 00:36:18 +0100

    Added function hook

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,56 @@
 #include <Windows.h>
 #include "Log.h"
 #include "Memory.h"
+#include <vector>
 Logger Log;
+
+
+std::vector<size_t> entity_array;
+
+
+
+void MyHook(uintptr_t* pThis, uintptr_t entity_address, DWORD* param_2)
+{
+	//std::cout << "[HOOK] Entity Address: 0x" << std::hex << (size_t)entity_address << std::dec << std::endl;
+
+	auto result = std::find(std::begin(entity_array), std::end(entity_array), (size_t)entity_address);
+	if (result == std::end(entity_array))
+	{
+		std::cout << "[HOOK] Added Entity! Address: 0x" << std::hex << (size_t)entity_address << std::dec << " [Total: "<< entity_array.size() <<"]" << std::endl;
+		float* x = (float*)(entity_address + 12);
+		float* y = (float*)(entity_address + 44);
+		float* z = (float*)(entity_address + 28);
+		std::cout << "Pos: (x: " << *x << ", y: " << *y << ", z: " << *z << ")\n";
+		entity_array.push_back((size_t)entity_address);
+
+		//if (entity_array.size() > 1000)
+		//	entity_array.pop_back();
+	}
+
+	
+	hook(pThis, entity_address, param_2);
+}
+
 
 void MainThread(HINSTANCE hinstDLL)
 {
 	std::cout << "Welcome! c:\n";
+
+
+	uintptr_t enemy_move_function = FindPattern("engine_x64_rwdi.dll",
+		"\x48\x89\x5C\x24\x00\x57\x48\x83\xEC\x20\x48\x8B\xFA\x48\x8B\xD9\xE8\x00\x00\x00\x00\x44\x8B\x5B\x28\x48\x8B\x43\x20\x48\x8B\x5C\x24\x00\x4C\x8B\x40\x40\x41\x81\xE3\x00\x00\x00\x00\x4B\x8D\x0C\x5B\x48\xC1\xE1\x04",
+		"xxxx?xxxxxxxxxxxx????xxxxxxxxxxxx?xxxxxxx????xxxxxxxx");
+	if (enemy_move_function)
+	{
+		std::cout << "[HOOKED] EnemyMove()\t\taddress: 0x" << std::hex << enemy_move_function << std::dec << std::endl;
+
+		if (!SetUp(enemy_move_function, (uintptr_t)MyHook, 16))
+			std::cout << "SetUp() failed\n";
+	}
+	else
+	{
+		std::cout << "Could not find EnemyMove() pattern.\n";
+	}
 
 	while (!GetAsyncKeyState(VK_F12) && 1);
 	FreeLibraryAndExitThread(hinstDLL, 0);
--- src/Memory.h
+++ src/Memory.h
@@ -4,6 +4,9 @@
 #include <vector>
 #include <Psapi.h>
 
+using fnHook = int(__fastcall*)(uintptr_t* pThis, uintptr_t entity_address, DWORD* param_2);
+fnHook hook;
+
 MODULEINFO GetModuleInfo(const char* szModule)
 {
     MODULEINFO modinfo = { 0 };
@@ -34,4 +37,63 @@ uintptr_t FindPattern(const char* module, const char* pattern, const char* mask)
             return base + i;
     }
     return 0;
+}
+
+uintptr_t GetRealAddress(BYTE* address)
+{
+	uintptr_t jmp = 0;
+	memcpy(&jmp, address + 0x1, 4);
+	address = address + jmp + 5;
+	return reinterpret_cast<uintptr_t>(address);
+}
+
+// can return hook function instead, eg (fnHook)hook = SetUp();
+bool SetUp(uintptr_t func_address, uintptr_t hook_address, size_t byte_size)
+{
+	void* page = nullptr;
+	page = VirtualAlloc(NULL, 1000, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	hook = (fnHook)page;
+	if (!page) return false;
+
+
+	// copy bytes
+	memcpy(page, reinterpret_cast<void*>(func_address), byte_size);
+	page = reinterpret_cast<void*>((uintptr_t)page + byte_size);
+
+	// add jmp to hook
+	const char jump_instruction[] = { 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 };
+	memcpy(page, jump_instruction, 6);
+	page = reinterpret_cast<void*>((uintptr_t)page + 6);
+
+	size_t func_address_after_bytes = func_address + byte_size;
+	memcpy(page, &func_address_after_bytes, 8);
+	page = reinterpret_cast<void*>((uintptr_t)page + 8);
+	void* page_after = page; // label
+
+
+	// jump to our hook
+	memcpy(page, jump_instruction, 6);
+	page = reinterpret_cast<void*>((uintptr_t)page + 6);
+
+	hook_address = GetRealAddress((BYTE*)hook_address);
+	size_t hook_func = hook_address;
+	memcpy(page, &hook_func, 8);
+	page = reinterpret_cast<void*>((uintptr_t)page + 8);
+
+
+	// patch main functions bytes
+	DWORD protecc;
+	VirtualProtect((size_t*)func_address, byte_size, PAGE_EXECUTE_READWRITE, &protecc);
+
+	memcpy((size_t*)func_address, jump_instruction, 6);
+	size_t page_reutnr = reinterpret_cast<size_t>(page_after);
+	memcpy((size_t*)(func_address + 6), &page_reutnr, 8);
+
+	const char nop[] = { 0x90 };
+	for (size_t i = 0; i < byte_size - 14; i++)
+		memcpy((size_t*)(func_address + 14 + i), nop, 1);
+
+	VirtualProtect((size_t*)func_address, byte_size, protecc, &protecc);
+
+	return true;
 }
